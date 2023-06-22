9c56447be3e9810a465c4acf2c800d5eaf0524a3
ara fa adri <arafa.sco@gmail.com>
2023-06-22 15:14:41 +0700

    chore: periode kerja dan route done

--- backend/controllers/PeriodeKerja.js
+++ backend/controllers/PeriodeKerja.js
@@ -1,22 +1,112 @@
 import PeriodeKerja from "../models/PeriodeKerjaModal.js";
+import Status from "../models/StatusModel.js";
 
 
-export const getPeriode = (req, res) => {
+export const getPeriode = async(req, res) => {
+    try {
+        const response = await PeriodeKerja.findAll({
+            attributes:['uuid','nameBulan','tanggalMulai','tanggalSelesai','jumlahHari'],
+            include:{
+                model:Status,
+                attributes:['uuid','name']
+            }
+        });
 
+        return res.status(200).json(response);
+    } catch (error) {
+        return res.status(500).json({msg: error.message})
+    }
 }
 
-export const getPeriodeById = (req, res) => {
+export const getPeriodeById = async(req, res) => {
+    try {
+        const response = await PeriodeKerja.findOne({
+            where:{
+                uuid:req.params.id
+            },
+            attributes:['uuid','nameBulan','tanggalMulai','tanggalSelesai','jumlahHari'],
+            include:{
+                model:Status,
+                attributes:['uuid','name']
+            }
+        });
 
+        return res.status(200).json(response);
+    } catch (error) {
+        return res.status(500).json({msg: error.message})
+    }
 }
 
-export const createPeriode = (req, res) => {
+export const createPeriode = async(req, res) => {
+    const {nameBulan, tanggalMulai, tanggalSelesai, jumlahHari, statusId} = req.body;
 
+    const status = await Status.findOne({
+        where:{
+            uuid:statusId
+        }
+    });
+
+    try {
+        await PeriodeKerja.create({
+            nameBulan:nameBulan,
+            tanggalMulai:tanggalMulai,
+            tanggalSelesai:tanggalSelesai,
+            jumlahHari:jumlahHari,
+            statusId:status && status.id
+        });
+
+        return res.status(201).json({msg: "success"});
+    } catch (error) {
+        return res.status(500).json({msg: error.message});
+    }
 }
 
-export const updatePeriode = (req, res) => {
+export const updatePeriode = async(req, res) => {
+    const {nameBulan, tanggalMulai, tanggalSelesai, jumlahHari, statusId} = req.body;
 
+    const findPeriode = await PeriodeKerja.findOne({
+        where:{
+            uuid:req.params.id
+        }
+    });
+
+    if(!findPeriode) return res.status(404).json({msg: "not found"});
+
+    const status = await Status.findOne({
+        where:{
+            uuid:statusId
+        }
+    });
+
+    try {
+        findPeriode.update({
+            nameBulan:nameBulan,
+            tanggalMulai:tanggalMulai,
+            tanggalSelesai:tanggalSelesai,
+            jumlahHari:jumlahHari,
+            statusId:status && status.id
+        });
+
+        return res.status(201).json({msg: "success"});
+    } catch (error) {
+        return res.status(500).json({msg: error.message});
+    }
 }
 
-export const deletePeriode = (req, res) => {
+export const deletePeriode = async(req, res) => {
+    const findPeriode = await PeriodeKerja.findOne({
+        where:{
+            uuid:req.params.id
+        }
+    });
 
+    if(!findPeriode) return res.status(404).json({msg: "not found"});
+
+    try {
+        findPeriode.destroy();
+
+        return res.status(201).json({msg: "success"});
+    } catch (error) {
+        return res.status(500).json({msg: error.message});
+    }
 }
--- backend/routes/PeriodeRoute.js
+++ backend/routes/PeriodeRoute.js
@@ -1,8 +1,18 @@
 import express from 'express';
-import { getPeriode } from '../controllers/PeriodeKerja.js';
+import { 
+    createPeriode, 
+    deletePeriode, 
+    getPeriode, 
+    getPeriodeById, 
+    updatePeriode 
+} from '../controllers/PeriodeKerja.js';
 
 const route = express.Router();
 
 route.get('/periode', getPeriode);
+route.get('/periode/:id', getPeriodeById);
+route.post('/periode', createPeriode);
+route.patch('/periode/:id', updatePeriode);
+route.delete('/periode/:id', deletePeriode);
 
-export default route
+export default route;
